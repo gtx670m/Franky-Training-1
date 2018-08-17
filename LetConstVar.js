0fb468f4d52ead2c28b88d549b18aab41aaf8030
Quang Le <quang.le@infonam.com>
2018-08-17 14:58:22 +0700

    init

--- LetConstVar.js
+++ LetConstVar.js
@@ -5,18 +5,27 @@ A = 10; // Lỗi Uncaught TypeError: Assignment to constant variable
 
 
 //let
+function foo() {
+    let x = 10;
+    if (true) {
+        let x = 20; // x này là x khác rồi đấy
+        console.log(x); // in ra 20
+    }
+    console.log(x); // in ra 10
+}
+
 
 
 
 //var
-console.log(a);
-var a = 123;
-console.log(a);
-// <=>
-var a;
-console.log(a);
-a = 123;
-console.log(a);
+function foo() {
+    var x = 10;
+    if (true) {
+        var x = 20; // x ở đây cũng là x ở trên
+        console.log(x); // in ra 20
+    }
+    console.log(x); // vẫn là 20
+}
 
 
 
