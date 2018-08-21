9b701d7ea4c41750cb79fb6d3c8ac4892b548922
Quang Le <quang.le@infonam.com>
2018-08-21 11:06:06 +0700

    init

--- 2/Debug.js
+++ 2/Debug.js
@@ -1,9 +1,6 @@
 export default {
-    //debug dùng để chuyển loader sang chế độ debug 
-    debug: true, //sẽ bị loại bỏ từ webpack 3 trở lên
+    debug: true, 
     plugins: [
-        //Để tương thích với với những loader cũ thì có thể 
-        //chuyển sang debug mode thông qua 1 plugin
         new webpack.LoaderOptionsPlugin({
             debug: true
         })
--- 2/test.js
+++ 2/test.js
@@ -1,15 +1,13 @@
-this.temp = 'Global';
+let temp = 40;
+this.temp = 20;
 const obj = {
-    temp: 'Local',
-    arrow: () => console.log(`arrow function: ${this.temp}`),
-    // this ở đây là this của function test
-    regular: function () {
-        // this.temp = 'Private';
-        console.log(`regular function: ${this.temp}`);
-        // this ở đây là object trả về
-    }
+    temp: 30,
+    test: function () {
+        this.temp = 10;
+        console.log(abc=this);
+    },
+    test2: () => { console.log(abc = this); }
 }
-obj.arrow(); // Global
-// => this ở arrow function tham chiếu đến cha gần nhất của nó
-//theo medium: arrow function sử dụng this từ đoạn mã chứa nó
-obj.regular(); // Local
+
+obj.test();
+// obj.test2();
