6eb7f56a7e0e15c5ac18578a8388e9e3ce412667
kenvinnguyen-terralogic <kenvin.nguyen@terralogic.com>
2018-10-23 15:04:48 +0700

    update

--- 2/Promise.js
+++ 2/Promise.js
@@ -31,13 +31,32 @@ const divide = (a, b) => {
 const subtract = (a, b) => {
     return new Promise((resolve, reject) => resolve(a - b));
 }
-add(4, 5).then(resultAdd => multiply(resultAdd, 6)
-.then(resultMul => divide(resultMul, 7)
-.then(resultDiv => subtract(resultDiv, 2)
-.then(resultSub => add(resultSub, 3)
-.then(resultAdd => subtract(resultAdd, 5)
-.then(resultSub => add(resultSub, 8)
-.then(resultAdd => subtract(resultAdd, 4)
-.then(resultSub => add(resultSub, 6)
-.then(resultAdd => console.log(resultAdd))
-))))))));
+
+// add(4, 5).then(resultAdd => multiply(resultAdd, 6)
+//     .then(resultMul => divide(resultMul, 7)
+//         .then(resultDiv => subtract(resultDiv, 2)
+//             .then(resultSub => add(resultSub, 3)
+//                 .then(resultAdd => subtract(resultAdd, 5)
+//                     .then(resultSub => add(resultSub, 8)
+//                         .then(resultAdd => subtract(resultAdd, 4)
+//                             .then(resultSub => add(resultSub, 6)
+//                                 .then(resultAdd => console.log(resultAdd))
+//                             )
+//                         )
+//                     )
+//                 )
+//             )
+//         )
+//     )
+// );
+
+add(4, 5).then(resultAdd => multiply(resultAdd, 6))
+    .then(resultMul => divide(resultMul, 7))
+    .then(resultDiv => subtract(resultDiv, 2))
+    .then(resultSub => add(resultSub, 3))
+    .then(resultAdd => subtract(resultAdd, 5))
+    .then(resultSub => add(resultSub, 8))
+    .then(resultAdd => subtract(resultAdd, 4))
+    .then(resultSub => add(resultSub, 6))
+    .then(resultAdd => console.log(resultAdd))
+    ;
